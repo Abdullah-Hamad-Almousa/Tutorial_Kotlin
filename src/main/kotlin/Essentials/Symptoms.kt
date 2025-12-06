4af904f9f8f609f9ad4d9562f83d7c8ccec65337
Abdullah almousa <abdullahmlwork@gmail.com>
2025-12-06 17:35:41 +0300

    more practice

--- src/main/kotlin/Essentials/Symptoms.kt
+++ src/main/kotlin/Essentials/Symptoms.kt
@@ -1,2 +1,19 @@
 package com.MysticByte.Essentials
 
+fun main(args: Array<String>) {
+
+    val symptoms: MutableList<String> = mutableListOf("white spots", "red spots", "not eating", "bloated", "belly up");
+
+    symptoms.add("white fungus");
+    symptoms.remove("white fungus");
+
+    symptoms.contains("red");
+    symptoms.contains("red spots");
+
+    println(symptoms.subList(4, symptoms.size)); //belly up
+
+    listOf(1, 3, 5).sum(); //9
+
+    listOf("a", "b", "cc").sumBy { it.length }; // 4
+
+}
